a80d89d6c58295b20f704c72a1506fe6c48687d5
AMN-47 <akhilnagar57@gmail.com>
2026-01-11 07:46:33 +0200

    Added variables in functions in JS File

--- Scripts/rpsc.js
+++ Scripts/rpsc.js
@@ -5,18 +5,20 @@ function game () {
     //play the game with computer
     //play five rounds
     //console based
-
+    playRound();
 } 
 
 function playRound () {
-    const playerSelection = 
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
 } 
 
 function playerChoice () {
     //player input
+    let input = prompt("Type Rock, Paper or Scissors")
 }
 
 function computerChoice (){
     //computer chooses random choice
-    return choices[Math.floor()]
+    return choices[Math.floor(Math.random()*choices.length)]
 }
