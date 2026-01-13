bb06f9c5ef962710ef40967c33d8e9e97b9123ad
AMN-47 <akhilnagar57@gmail.com>
2026-01-13 09:21:15 +0200

    Added while loop for null input on popup to prevent code from crashing when clicking on cancel or leaving input blank

--- Scripts/rpsc.js
+++ Scripts/rpsc.js
@@ -16,6 +16,9 @@ function playRound () {
 function playerChoice () {
     //player input
     let input = prompt("Type Rock, Paper or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper or Scissors");
+    }
     input = input.toLowerCase();
     console.log(input);
 }
