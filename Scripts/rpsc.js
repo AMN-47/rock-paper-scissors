c04fbb56fa66220503d09ae39e5aee538f03d39e
AMN-47 <akhilnagar57@gmail.com>
2026-01-13 09:27:08 +0200

    added error handling to prevent incorrect input from being logged into the console

--- Scripts/rpsc.js
+++ Scripts/rpsc.js
@@ -20,12 +20,26 @@ function playerChoice () {
         input = prompt("Type Rock, Paper or Scissors");
     }
     input = input.toLowerCase();
-    console.log(input);
+    let check = validateInput(input)
+    if (check == true){
+        console.log(input)
+    }
+    //console.log(input);
 }
 
 function computerChoice (){
     //computer chooses random choice
     return choices[Math.floor(Math.random()*choices.length)]
 } 
+
+function validateInput(choice) {
+    //this function ensures that there is error handeling when wrong choice/input is given
+    if(choices.includes(choice)){
+        return true
+    }else{
+        return false
+    }
+}
+
 
 game();
